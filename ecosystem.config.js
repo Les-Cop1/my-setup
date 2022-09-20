c65a178df6d29f5b4e0afda717c035c5fa87eaa2
Lucas Stoebner <stoebnerl@gmail.com>
2022-09-20 15:40:40 +0200

    :tada: first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+module.exports = {
+  deploy: {
+    production: {
+      key: 'deploy.key',
+      user: process.env.HOST_USER,
+      host: [
+        {
+          host: process.env.HOST,
+          port: process.env.HOST_PORT,
+        },
+      ],
+      ssh_options: ['StrictHostKeyChecking=no'],
+      ref: 'origin/main',
+      repo: 'git@github.com:Les-Cop1/my-setup.git',
+      path: process.env.HOST_PATH,
+      'post-deploy': 'yarn setup && yarn build',
+      env: {},
+    },
+  },
+}
---
+++ src/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import LanguageDetector from 'i18next-browser-languagedetector'
+import en from './assets/locales/en.json'
+import fr from './assets/locales/fr.json'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    resources: { fr, en },
+    fallbackLng: 'en',
+    debug: process.env.NODE_ENV === 'development',
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  })
+
+export default i18n
