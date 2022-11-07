82b542dea63f7018d5311e936fe72cc31885f42c
Lucas Stoebner <stoebnerl@gmail.com>
2022-11-07 09:18:55 +0100

    :sparkles: Add image when creating item

--- src/components/item/add-item.tsx
+++ src/components/item/add-item.tsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 
 import { createItem } from '@api'
 import { Button, ButtonVariant, Input, Select, SelectOptionProps, SlideOver, Upload } from '@components'
-import { IRoom } from '@types'
+import { TrashIcon } from '@heroicons/react/24/outline'
+import { IRoom, isRegisteredFile } from '@types'
 
+import { getBaseURL } from '../../setupAxios'
 import { useTranslation } from 'react-i18next'
 
 type AddItemProps = {
@@ -25,6 +27,7 @@ export const AddItem: React.FC<AddItemProps> = ({ isOpen, onClose, getRooms, cat
   const [link, setLink] = useState<string>('')
   const [categories, setCategories] = useState<any>([])
   const [description, setDescription] = useState<string>('')
+  const [image, setImage] = useState<File | undefined>(undefined)
   const [invoice, setInvoice] = useState<File | undefined>(undefined)
 
   const handleClose = () => {
@@ -35,6 +38,7 @@ export const AddItem: React.FC<AddItemProps> = ({ isOpen, onClose, getRooms, cat
     setLink('')
     setCategories([])
     setDescription('')
+    setImage(undefined)
     setInvoice(undefined)
     onClose()
   }
@@ -47,6 +51,7 @@ export const AddItem: React.FC<AddItemProps> = ({ isOpen, onClose, getRooms, cat
       purchaseDate: purchaseDate?.toString(),
       link,
       description,
+      image,
       invoice,
       categories: [],
       room: room?._id,
@@ -66,6 +71,22 @@ export const AddItem: React.FC<AddItemProps> = ({ isOpen, onClose, getRooms, cat
     <SlideOver isOpen={isOpen} onClose={onClose} title={t('Add item')}>
       <div className="flex flex-col h-full justify-between">
         <div>
+          <div className="pb-5">
+            <div className="flex items-center">
+              <span className="h-12 w-12 overflow-hidden rounded-full bg-gray-100 relative">
+                {image && <img src={URL.createObjectURL(image.name === '' ? new Blob() : image)} alt={t('Item')} />}
+              </span>
+              <Upload
+                type="button"
+                name="image"
+                label={t(isRegisteredFile(image) ? 'Update the image' : 'Upload an image')}
+                file={!isRegisteredFile(image) ? image : undefined}
+                accept="image/png, image/jpeg"
+                onFileSelect={(e) => setImage(e)}
+                onFileSelectError={(e) => console.error(e)}
+              />
+            </div>
+          </div>
           <div className="pb-5">
             <Input
               label={t('Brand')}
